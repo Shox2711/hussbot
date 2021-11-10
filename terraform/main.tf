ad1e96dcf8101b8144c83245d6c713d6ed63ca0e
Cian O'Shaughnessy <oshaughnessycian@gmail.com>
2021-11-10 21:26:21 +0000

    variablising + move pub key to secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,7 +83,7 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "hussbot-tf-locks"
+  name         = var.db_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"  
   
