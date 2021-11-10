c1fe218744f7bc5e4818a442c547e2eadccf42ba
Cian O'Shaughnessy <oshaughnessycian@gmail.com>
2021-11-10 21:15:55 +0000

    Added s3 storage bucket & dynamodb for tf backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,14 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket         = "hussbot-tf"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "hussbot-tf-locks"
+    encrypt        = true
+  }
+
   required_version = ">= 0.14.9"
 }
 
@@ -58,6 +66,31 @@ resource "aws_security_group" "main" {
 resource "aws_key_pair" "ssh-key" {
   key_name   = "ssh-key"
   public_key = var.public_key
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "hussbot-tf"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "hussbot-tf-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"  
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
 output "instance_ip" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,11 @@ variable "instance_type" {
 variable "public_key" {
     default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCu/yECoa8pHgLejCuNQrn0CWQB/jQbpALPbf/ZUhbEhbDPNU8lFOIZ9lKvFHnhM/dTq6qVILPJiJ653K1OwjHWfng7HnQfJ46I5qR2SJQo91phJKHhXuPkPVg/geWfE5QI6tNqK/YTRSabn1iSk7Ad0mmpTD2jdadtu/mTooTraP3ge56Xw9OfvvhAteOeqMwu4w7TeQ3tTi/jaWnAvPvpg+PB/Q84vYFgwJB+58pll6RHjRvCgCaB0WQovpV4OcCHqUmkh7Z3iGIan9XJgjyMTBWkkt1fgvvgM7FE6Bx/bD4hRL9sZwBErAWCimrollUaTfWx5+qTNoQya34y/RKJY8IQtTX0g009D59CLtnbGzIoradg4+uD46FntPKBxIYmlZMLzB33lJ6muQro1XvspI+RnNXuBjZybEDoLBX3m4Rm09Q9nqX5KUos/FzeqIXp3x95+nCOBZ9H6n31H9/+PjwgmrhvFscl50lgNRaDXw/xbq3VOd4ixccoDMswE8YAQXIUUNqk6Ie7qgMWCtlMw3ONm+u3MNA6JCjRlIroYxM+CKSqNWHEhME+W4fRiGxFef8wNBPaYIxHxPTKIAftlb6UzlD/4grFWwL8g1ZyHWtqw/3wC5V9dP1j2e9UGBR/XE2QbkK8rzGgLTDyJ41s7LUcXsZyxu/3jvqtWdJCjQ== shox@DESKTOP-JR22FJS"
 }
+
+variable "bucket_name" {
+    default = "hussbot-tf"
+}
+
+variable "acl_value" {
+    default = "private"
+}
